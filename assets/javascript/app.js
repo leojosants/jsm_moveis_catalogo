aa9540f8468cba0999ae3d618ab32cad91abfd97
Leonardo Santos <leonardojosantos@gmail.com>
2024-05-09 19:55:05 -0300

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,95 @@
+import { dataCards } from './database/cardsDB.js';
+
+const container_all_cards = document.querySelector('[data-container-all-cards]');
+
+const renderImages = () => {
+    dataCards.map(
+        (product) => {
+            const element_card = document.createElement('div');
+
+            element_card.setAttribute('data-cards', '');
+            element_card.setAttribute('data-category', product.category);
+            element_card.setAttribute('data-id', product.id);
+
+            element_card.innerHTML = `
+            <img class="c-post-img" src="${product.src}" alt="${product.alt}"/>
+            <h2>${product.title}</h2>
+            <p>ref: ${product.id}</p>
+            <p>
+               R$<span>${product.price}</span>
+            </p>
+            <p>${product.description}</p>
+        `;
+
+            container_all_cards.appendChild(element_card);
+        }
+    );
+};
+
+const showCategory = (allCards, productCategory) => {
+    allCards.forEach(
+        (data) => {
+            if (data.dataset.category === `${productCategory}`) {
+                data.style.display = 'block';
+            }
+            else {
+                data.style.display = 'none';
+            }
+        }
+    );
+};
+
+window.addEventListener('click',
+    (event) => {
+        const eventTarget = event.target;
+        const filteredCategory = eventTarget.dataset.filter;
+        const allCards = document.querySelectorAll('[data-cards]');
+
+        switch (filteredCategory) {
+            case 'all_categories':
+                allCards.forEach(
+                    (data) => {
+                        data.style.display = 'block';
+                    }
+                );
+                break;
+
+            case 'sofa_category':
+                showCategory(allCards, 'sofa');
+                break;
+
+            case 'mesa_category':
+                showCategory(allCards, 'mesa');
+                break;
+
+            case 'cadeira_category':
+                showCategory(allCards, 'cadeira');
+                break;
+
+            case 'cama_category':
+                showCategory(allCards, 'cama');
+                break;
+
+            case 'armario_category':
+                showCategory(allCards, 'armario');
+                break;
+
+            case 'banco_category':
+                showCategory(allCards, 'banco');
+                break;
+
+            case 'mesa_com_cadeira_category':
+                showCategory(allCards, 'mesa_com_cadeira');
+                break;
+
+            default:
+                break;
+        }
+    }
+);
+
+window.addEventListener('DOMContentLoaded',
+    () => {
+        renderImages();
+    }
+);
---
+++ assets/javascript/database/cardsDB.js
@@ -0,0 +1,119 @@
+export const dataCards = [
+    {
+        id: 1,
+        title: 'Produto 1',
+        src: '/assets/images/banco/01.jpeg',
+        category: 'banco',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 2,
+        title: 'Produto 2',
+        src: '/assets/images/cadeira/02.jpeg',
+        alt: 'produto 2',
+        category: 'cadeira',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 3,
+        title: 'Produto 3',
+        src: '/assets/images/banco/03.jpeg',
+        alt: 'produto 3',
+        category: 'banco',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 4,
+        title: 'Produto 4',
+        src: './assets/images/04.jpeg',
+        src: '/assets/images/banco/04.jpeg',
+        alt: 'produto 4',
+        category: 'banco',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 5,
+        title: 'Produto 5',
+        src: '/assets/images/armario/05.jpeg',
+        alt: 'produto 5',
+        category: 'armario',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 6,
+        title: 'Produto 6',
+        src: '/assets/images/mesa_com_cadeira/06.jpeg',
+        alt: 'produto 6',
+        category: 'mesa_com_cadeira',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 7,
+        title: 'Produto 7',
+        src: '/assets/images/banco/07.jpeg',
+        alt: 'produto 7',
+        category: 'banco',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 8,
+        title: 'Produto 8',
+        src: '/assets/images/armario/08.jpeg',
+        alt: 'produto 8',
+        category: 'armario',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 9,
+        title: 'Produto 9',
+        src: '/assets/images/armario/09.jpeg',
+        alt: 'produto 9',
+        category: 'armario',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 10,
+        title: 'Produto 10',
+        src: '/assets/images/mesa/10.jpeg',
+        alt: 'produto 10',
+        category: 'mesa',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 11,
+        title: 'Produto 11',
+        src: '/assets/images/cadeira/11.jpeg',
+        alt: 'produto 11',
+        category: 'cadeira',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 12,
+        title: 'Produto 12',
+        src: '/assets/images/mesa/12.jpeg',
+        alt: 'produto 12',
+        category: 'mesa',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+    {
+        id: 13,
+        title: 'Produto 13',
+        src: '/assets/images/armario/13.jpeg',
+        alt: 'produto 13',
+        category: 'armario',
+        description: 'Descrição do produto',
+        price: 99.00
+    },
+];
