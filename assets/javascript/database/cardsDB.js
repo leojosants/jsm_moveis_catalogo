04e78532439ff007b0fa3286cd0eed2525eac45e
Leonardo Santos <leonardojosantos@gmail.com>
2024-05-09 20:04:43 -0300

    atualizado caminho das imagens

--- assets/javascript/database/cardsDB.js
+++ assets/javascript/database/cardsDB.js
@@ -2,7 +2,8 @@ export const dataCards = [
     {
         id: 1,
         title: 'Produto 1',
-        src: '/assets/images/banco/01.jpeg',
+        src: './assets/images/banco/01.jpeg',
+        alt: 'Produto 1',
         category: 'banco',
         description: 'Descrição do produto',
         price: 99.00
@@ -10,7 +11,7 @@ export const dataCards = [
     {
         id: 2,
         title: 'Produto 2',
-        src: '/assets/images/cadeira/02.jpeg',
+        src: './assets/images/cadeira/02.jpeg',
         alt: 'produto 2',
         category: 'cadeira',
         description: 'Descrição do produto',
@@ -19,7 +20,7 @@ export const dataCards = [
     {
         id: 3,
         title: 'Produto 3',
-        src: '/assets/images/banco/03.jpeg',
+        src: './assets/images/banco/03.jpeg',
         alt: 'produto 3',
         category: 'banco',
         description: 'Descrição do produto',
@@ -28,8 +29,8 @@ export const dataCards = [
     {
         id: 4,
         title: 'Produto 4',
-        src: './assets/images/04.jpeg',
-        src: '/assets/images/banco/04.jpeg',
+        src: '../assets/images/04.jpeg',
+        src: './assets/images/banco/04.jpeg',
         alt: 'produto 4',
         category: 'banco',
         description: 'Descrição do produto',
@@ -38,7 +39,7 @@ export const dataCards = [
     {
         id: 5,
         title: 'Produto 5',
-        src: '/assets/images/armario/05.jpeg',
+        src: './assets/images/armario/05.jpeg',
         alt: 'produto 5',
         category: 'armario',
         description: 'Descrição do produto',
@@ -47,7 +48,7 @@ export const dataCards = [
     {
         id: 6,
         title: 'Produto 6',
-        src: '/assets/images/mesa_com_cadeira/06.jpeg',
+        src: './assets/images/mesa_com_cadeira/06.jpeg',
         alt: 'produto 6',
         category: 'mesa_com_cadeira',
         description: 'Descrição do produto',
@@ -56,7 +57,7 @@ export const dataCards = [
     {
         id: 7,
         title: 'Produto 7',
-        src: '/assets/images/banco/07.jpeg',
+        src: './assets/images/banco/07.jpeg',
         alt: 'produto 7',
         category: 'banco',
         description: 'Descrição do produto',
@@ -65,7 +66,7 @@ export const dataCards = [
     {
         id: 8,
         title: 'Produto 8',
-        src: '/assets/images/armario/08.jpeg',
+        src: './assets/images/armario/08.jpeg',
         alt: 'produto 8',
         category: 'armario',
         description: 'Descrição do produto',
@@ -74,7 +75,7 @@ export const dataCards = [
     {
         id: 9,
         title: 'Produto 9',
-        src: '/assets/images/armario/09.jpeg',
+        src: './assets/images/armario/09.jpeg',
         alt: 'produto 9',
         category: 'armario',
         description: 'Descrição do produto',
@@ -83,7 +84,7 @@ export const dataCards = [
     {
         id: 10,
         title: 'Produto 10',
-        src: '/assets/images/mesa/10.jpeg',
+        src: './assets/images/mesa/10.jpeg',
         alt: 'produto 10',
         category: 'mesa',
         description: 'Descrição do produto',
@@ -92,7 +93,7 @@ export const dataCards = [
     {
         id: 11,
         title: 'Produto 11',
-        src: '/assets/images/cadeira/11.jpeg',
+        src: './assets/images/cadeira/11.jpeg',
         alt: 'produto 11',
         category: 'cadeira',
         description: 'Descrição do produto',
@@ -101,7 +102,7 @@ export const dataCards = [
     {
         id: 12,
         title: 'Produto 12',
-        src: '/assets/images/mesa/12.jpeg',
+        src: './assets/images/mesa/12.jpeg',
         alt: 'produto 12',
         category: 'mesa',
         description: 'Descrição do produto',
@@ -110,7 +111,7 @@ export const dataCards = [
     {
         id: 13,
         title: 'Produto 13',
-        src: '/assets/images/armario/13.jpeg',
+        src: './assets/images/armario/13.jpeg',
         alt: 'produto 13',
         category: 'armario',
         description: 'Descrição do produto',
